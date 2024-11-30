ab7c9e5dd23dfa26bd9f5f41c4668b11f563e485
Oscar Marin <oscar@marin.cr>
2024-11-29 20:57:48 -0600

    improvements

--- src/Luval.SnapBite.Web/Program.cs
+++ src/Luval.SnapBite.Web/Program.cs
@@ -21,8 +21,6 @@ namespace Luval.SnapBite.Web
                 .AddInteractiveServerComponents();
 
 
-            /* BEGIN CUSTOM CONFIGURATION */
-
             // Add controllers
             builder.Services.AddControllers();
             builder.Services.AddHttpClient();
@@ -30,25 +28,30 @@ namespace Luval.SnapBite.Web
 
             // Configure AuthMate
             var dbContext = new PostgresAuthMateContext(ConfigHelper.GetValueAsString("ConnectionString:Authorization"));
-            
+
             var authService = new AuthMateService(
                     dbContext,
                     "Free", "Administrator"
                 );
 
+
+            // Function to be called after the user is authorized by Google
             Func<OAuthCreatingTicketContext, Task> onTicket = async contex =>
             {
                 await authService.OnUserAuthorizedAsync(contex.Identity, "Google", null);
             };
 
-            // Add Google Authentication
-            builder.Services.AddGoogleAuth(new GoogleOAuthConfiguration() { 
+            // Add Google Authentication configuration
+            builder.Services.AddGoogleAuth(new GoogleOAuthConfiguration()
+            {
+                // client id from your config file
                 ClientId = ConfigHelper.GetValueAsString("Authentication:Google:ClientID"),
+                // the client secret from your config file
                 ClientSecret = ConfigHelper.GetValueAsString("Authentication:Google:ClientSecret"),
-                OnCreatingTicket = onTicket
+                OnCreatingTicket = onTicket // function call
             });
 
-            /* END CUSTOM CONFIGURATION */
+
 
             var app = builder.Build();
 
@@ -62,18 +65,18 @@ namespace Luval.SnapBite.Web
 
             app.UseHttpsRedirection();
 
-            /*******/
+            /*** Adds support for controllers     ****/
             app.MapControllers();
             app.MapBlazorHub();
             app.UseRouting();
             app.UseAuthorization();
             app.UseAuthentication();
-            /*******/
+            /*** End code to suupport controllers ****/
 
             app.UseStaticFiles();
             app.UseAntiforgery();
 
-           
+
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
