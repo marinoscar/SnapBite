548ddd6c581d16dce57d2fae7f7428afbced46de
Oscar Marin <oscar@marin.cr>
2024-12-01 19:53:34 -0600

    improvements

--- src/Luval.SnapBite.Fluent/HttpExtensions.cs
+++ src/Luval.SnapBite.Fluent/HttpExtensions.cs
@@ -12,33 +12,6 @@ namespace Luval.SnapBite.Fluent
                 c.HttpContext.User.Identity.IsAuthenticated;
         }
 
-        public static AppUser ToUser(this ClaimsPrincipal identity)
-        {
-            return ((ClaimsIdentity)identity.Identity).ToUser();
-        }
-
-        public static AppUser ToUser(this ClaimsIdentity identity)
-        {
-            return new AppUser()
-            {
-                ProviderType = identity.GetValue("providerName"),
-                ProviderKey = identity.GetValue(ClaimTypes.NameIdentifier),
-                DisplayName = identity.GetValue(ClaimTypes.Name),
-                Email = identity.GetValue(ClaimTypes.Email),
-                ProfilePictureUrl = identity.GetValue("urn:google:image")
-            };
-        }
-
-        public static string? GetValue(this ClaimsIdentity c, string type)
-        {
-            if (c == null) return null;
-            if (!c.HasClaim(i => i.Type == type)) return null;
-            return c.Claims.First(i => i.Type == type).Value;
-        }
-
-        public static string? GetValue(this ClaimsPrincipal c, string type)
-        {
-            return ((ClaimsIdentity)c.Identity).GetValue(type);
-        }
+        
     }
 }
